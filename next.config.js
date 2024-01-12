545968560d38b6ff04c5d5bf485d1081e1b8a266
SHINYOONCHAN <tlsdbscks123@gmail.com>
2024-01-13 08:41:06 +0900

    fixed config file

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  // output: 'export',
-  // images: {
-  //   unoptimized: true,
-  // },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
+
+
+
